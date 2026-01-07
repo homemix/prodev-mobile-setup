ac7062e10aa1f0b4eeb737f9415743f57c0916b6
homemix <kenmutati@gmail.com>
2026-01-07 11:56:05 +0300

    EDIT: Explore More Core Components checker compliance.

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,7 +1,7 @@
-import {Text, TextInput, View, TouchableOpacity, Image} from "react-native";
-import {styles} from "@/styles";
-import {SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context";
-import {FontAwesome, Ionicons} from "@expo/vector-icons";
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
     return (
