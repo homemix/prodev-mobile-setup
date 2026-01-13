bed4b23488a2844708b47940e8d09b1184639c35
homemix <kenmutati@gmail.com>
2026-01-13 10:42:58 +0300

    ADD: Navigation using Stack Navigation Patterns

---
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -0,0 +1,104 @@
+import { Dimensions, ImageBackground, Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Image } from "expo-image";
+import {router} from "expo-router";
+
+export default function Index() {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <ImageBackground
+                    source={require("@/assets/images/background-image.png")}
+                    style={styles.background}
+                    resizeMode="cover"
+                >
+                    <View style={styles.companyLogo}>
+                        <Image source={require("@/assets/images/Logo.png")}/>
+                    </View>
+                    <View style={styles.textGroup}>
+                        <Text style={styles.textLarge}>Find your favorite place here</Text>
+                        <Text style={styles.textSmall}>The best prices for over 2 </Text>
+                        <Text style={styles.textSmall}>million properties worldwide</Text>
+                    </View>
+
+                    <View style={styles.buttonGroup}>
+                        <TouchableOpacity
+                            onPress={()=> router.push('/join')}
+                            style={styles.button}
+                        >
+                            <Text style={{...styles.textSmall, color: "black"}}>Join here</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.transparentButton}
+                        onPress={()=>router.push('/signin')}>
+                            <Text style={styles.textSmall}>Sign In</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{alignItems: "center",paddingVertical:20}}>
+                        <Text style={{color: "white"}}>Continue to home</Text>
+                    </View>
+                </ImageBackground>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+        justifyContent: "center",
+        width: "100%",
+        height: Dimensions.get("window").height,
+    },
+    companyLogo: {
+        width: "100%",
+        alignItems: "center",
+        padding: 20,
+        marginBottom: 50,
+    },
+    textGroup: {
+        alignItems: "center",
+    },
+    textLarge: {
+        color: "white",
+        fontWeight: "800",
+        fontSize: 40,
+        textAlign: "center",
+        marginBottom: 12,
+    },
+    textSmall: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "200",
+        textAlign: "center",
+    },
+    transparentButton: {
+        borderColor: "white",
+        borderWidth: 2,
+        borderRadius: 40,
+        paddingVertical: 15,
+        paddingHorizontal: 5,
+        alignItems: "center",
+        fontSize: 20,
+        flex: 1,
+    },
+    button: {
+        borderColor: "white",
+        borderWidth: 2,
+        borderRadius: 40,
+        paddingVertical: 15,
+        paddingHorizontal: 5,
+        alignItems: "center",
+        fontSize: 20,
+        backgroundColor: "white",
+        flex: 1,
+    },
+    buttonGroup: {
+        flexDirection: "row",
+        gap: 20,
+        paddingHorizontal: 20,
+    },
+});
